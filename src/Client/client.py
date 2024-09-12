d89d6566afe716bd1cf31c057cdb21bcbeda66ac
derrick921213 <dlin12457@gmail.com>
2024-09-13 02:14:48 +0800

    feat: basic upload/download

--- src/Client/client.py
+++ src/Client/client.py
@@ -1,62 +1,111 @@
+# import grpc
+# from proto.recommendations_pb2 import BookCategory, RecommendationRequest
+# from proto.recommendations_pb2_grpc import RecommendationsStub
+# import sys
+# from grpc import FutureTimeoutError
+
+
+# class RecommendationClient:
+#     def __init__(self, server_address):
+#         """
+#         初始化客戶端類，設定 gRPC 服務端位址並建立連線。
+#         """
+#         self.server_address = server_address
+#         self.channel = None
+#         self.client = None
+
+#     def connect(self, timeout=5):
+#         """
+#         建立與 gRPC 伺服器的連接，等待通道準備好後返回。
+#         """
+#         try:
+#             self.channel = grpc.insecure_channel(self.server_address)
+#             grpc.channel_ready_future(self.channel).result(timeout=timeout)
+#             self.client = RecommendationsStub(self.channel)
+#             print(f"Connected to gRPC server at {self.server_address}")
+#         except FutureTimeoutError:
+#             print(f"Failed to connect to gRPC server at {
+#                   self.server_address} (timeout after {timeout} seconds)")
+#             sys.exit(1)
+#         except grpc.RpcError as e:
+#             print(f"Failed to connect to gRPC server: {e.details()}")
+#             sys.exit(1)
+
+#     def get_recommendations(self, user_id, category, max_results):
+#         """
+#         發起推薦請求並傳回結果。
+
+#         :param user_id: 用戶 ID
+#         :param category: 圖書類別
+#         :param max_results: 最大返回數量
+#         :return: 推薦結果
+#         """
+#         request = RecommendationRequest(
+#             user_id=user_id,
+#             category=category,
+#             max_results=max_results
+#         )
+#         return self.client.Recommend(request)
+
+#     def close(self):
+#         """
+#         關閉連接。
+#         """
+#         if self.channel:
+#             self.channel.close()
+#             print("Connection closed.")
+
+
+# # if __name__ == "__main__":
+# def client(args: list[str]):
+#     host: str = 'localhost'
+#     port: str = "50051"
+#     if len(args) == 1:
+#         import json
+#         data: dict[str, str] = json.loads(args[0])
+#         if 'host' in data:
+#             host = data['host']
+#         if 'port' in data:
+#             port = data['port']
+#     connect_address: str = f"{host}:{port}"
+#     client = RecommendationClient(connect_address)
+#     client.connect()
+#     try:
+#         response = client.get_recommendations(
+#             user_id=1,
+#             category=BookCategory.SCIENCE_FICTION,
+#             max_results=3
+#         )
+#         print(response)
+#     finally:
+#         client.close()
+
+import os
 import grpc
-from proto.recommendations_pb2 import BookCategory, RecommendationRequest
-from proto.recommendations_pb2_grpc import RecommendationsStub
-import sys
-from grpc import FutureTimeoutError
+from proto import hello_pb2_grpc, hello_pb2
 
 
-class RecommendationClient:
-    def __init__(self, server_address):
-        """
-        初始化客戶端類，設定 gRPC 服務端位址並建立連線。
-        """
-        self.server_address = server_address
-        self.channel = None
-        self.client = None
-
-    def connect(self, timeout=5):
-        """
-        建立與 gRPC 伺服器的連接，等待通道準備好後返回。
-        """
-        try:
-            self.channel = grpc.insecure_channel(self.server_address)
-            grpc.channel_ready_future(self.channel).result(timeout=timeout)
-            self.client = RecommendationsStub(self.channel)
-            print(f"Connected to gRPC server at {self.server_address}")
-        except FutureTimeoutError:
-            print(f"Failed to connect to gRPC server at {
-                  self.server_address} (timeout after {timeout} seconds)")
-            sys.exit(1)
-        except grpc.RpcError as e:
-            print(f"Failed to connect to gRPC server: {e.details()}")
-            sys.exit(1)
-
-    def get_recommendations(self, user_id, category, max_results):
-        """
-        發起推薦請求並傳回結果。
-
-        :param user_id: 用戶 ID
-        :param category: 圖書類別
-        :param max_results: 最大返回數量
-        :return: 推薦結果
-        """
-        request = RecommendationRequest(
-            user_id=user_id,
-            category=category,
-            max_results=max_results
-        )
-        return self.client.Recommend(request)
-
-    def close(self):
-        """
-        關閉連接。
-        """
-        if self.channel:
-            self.channel.close()
-            print("Connection closed.")
+def read_iterfile(file_path: str, chunk_size: int = 1024):
+    split_data = os.path.splitext(file_path.split('/')[-1])
+    filename = split_data[0]
+    extension = split_data[1]
+    metadata = hello_pb2.MetaData(filename=filename, extension=extension)
+    print(f'{filename=}, {extension=}')
+    yield hello_pb2.UploadFileRequest(metadata=metadata)
+    with open(file_path, 'rb') as f:
+        while True:
+            chunk = f.read(chunk_size)
+            if chunk:
+                entry_request = hello_pb2.UploadFileRequest(chunk_data=chunk)
+                yield entry_request
+            else:  # The chunk was empty, which means we're at the end of the file
+                break
 
 
-# if __name__ == "__main__":
+def get_filepath(filename, extension):
+    return f'{filename}{extension}'
+
+
 def client(args: list[str]):
     host: str = 'localhost'
     port: str = "50051"
@@ -68,14 +117,24 @@ def client(args: list[str]):
         if 'port' in data:
             port = data['port']
     connect_address: str = f"{host}:{port}"
-    client = RecommendationClient(connect_address)
-    client.connect()
-    try:
-        response = client.get_recommendations(
-            user_id=1,
-            category=BookCategory.SCIENCE_FICTION,
-            max_results=3
-        )
-        print(response)
-    finally:
-        client.close()
+    with grpc.insecure_channel(connect_address) as channel:
+        stub = hello_pb2_grpc.GreeterStub(channel)
+        response = stub.SayHello(hello_pb2.HelloRequest(name='you', age=18))
+        print("Greeter client received: " + response.message)
+        src_dir: str = os.path.dirname(os.path.dirname(__file__))
+        data_dir: str = os.path.join(src_dir, "Data")
+        uploads_dir: str = os.path.join(data_dir, "uploads")
+        downloads_dir: str = os.path.join(data_dir, "downloads")
+        ret = stub.UploadFile(read_iterfile(
+            os.path.join(data_dir, "info.txt")))
+        # read_iterfile(
+        #     os.path.join(data_dir, "info.txt"))
+        print("Greeter client received: " + ret.message)
+
+        filename = 'info'
+        extension = '.txt'
+        filepath = get_filepath(filename, extension)
+        for entry_response in stub.DownloadFile(hello_pb2.MetaData(filename=filename, extension=extension)):
+            with open(os.path.join(downloads_dir, filepath), mode="ab") as f:
+                f.write(entry_response.chunk_data)
+            # print(entry_response)
--- src/Server/server.py
+++ src/Server/server.py
@@ -1,49 +1,115 @@
+# from concurrent import futures
+# import random
+# import grpc
+# from proto.recommendations_pb2 import BookCategory, BookRecommendation, RecommendationResponse
+# from proto.recommendations_pb2_grpc import RecommendationsServicer, add_RecommendationsServicer_to_server
+
+
+# class RecommendationService(RecommendationsServicer):
+#     def Recommend(self, request, context):
+#         if request.category not in books_by_category:
+#             context.abort(grpc.StatusCode.NOT_FOUND, "Category not found")
+
+#         books_for_category = books_by_category[request.category]
+#         num_results = min(request.max_results, len(books_for_category))
+#         books_to_recommend = random.sample(
+#             books_for_category, num_results
+#         )
+
+#         return RecommendationResponse(recommendations=books_to_recommend)
+
+
+# books_by_category = {
+#     BookCategory.MYSTERY: [
+#         BookRecommendation(id=1, title="The Maltese Falcon"),
+#         BookRecommendation(id=2, title="Murder on the Orient Express"),
+#         BookRecommendation(id=3, title="The Hound of the Baskervilles"),
+#     ],
+#     BookCategory.SCIENCE_FICTION: [
+#         BookRecommendation(
+#             id=4, title="The Hitchhiker's Guide to the Galaxy"
+#         ),
+#         BookRecommendation(id=5, title="Ender's Game"),
+#         BookRecommendation(id=6, title="The Dune Chronicles"),
+#     ],
+#     BookCategory.SELF_HELP: [
+#         BookRecommendation(
+#             id=7, title="The 7 Habits of Highly Effective People"
+#         ),
+#         BookRecommendation(
+#             id=8, title="How to Win Friends and Influence People"
+#         ),
+#         BookRecommendation(id=9, title="Man's Search for Meaning"),
+#     ],
+# }
+
+# # 將剛剛實作完的service架起來
+
+
+# def server(args: list[str]):
+#     host: str = '[::]'
+#     port: str = "50051"
+#     if len(args) == 1:
+#         import json
+#         data: dict[str, str] = json.loads(args[0])
+#         if 'host' in data:
+#             host = data['host']
+#         if 'port' in data:
+#             port = data['port']
+#     connect_address: str = f"{host}:{port}"
+
+#     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+#     add_RecommendationsServicer_to_server(
+#         RecommendationService(), server
+#     )
+#     print(f"Server start at {connect_address}")
+#     server.add_insecure_port(connect_address)
+#     server.start()
+#     server.wait_for_termination()
+
 from concurrent import futures
-import random
+import os
 import grpc
-from proto.recommendations_pb2 import BookCategory, BookRecommendation, RecommendationResponse
-from proto.recommendations_pb2_grpc import RecommendationsServicer, add_RecommendationsServicer_to_server
+from proto import hello_pb2, hello_pb2_grpc
 
 
-class RecommendationService(RecommendationsServicer):
-    def Recommend(self, request, context):
-        if request.category not in books_by_category:
-            context.abort(grpc.StatusCode.NOT_FOUND, "Category not found")
-
-        books_for_category = books_by_category[request.category]
-        num_results = min(request.max_results, len(books_for_category))
-        books_to_recommend = random.sample(
-            books_for_category, num_results
-        )
-
-        return RecommendationResponse(recommendations=books_to_recommend)
+def get_filepath(filename: str, extension: str) -> str:
+    return f"{filename}{extension}"
 
 
-books_by_category = {
-    BookCategory.MYSTERY: [
-        BookRecommendation(id=1, title="The Maltese Falcon"),
-        BookRecommendation(id=2, title="Murder on the Orient Express"),
-        BookRecommendation(id=3, title="The Hound of the Baskervilles"),
-    ],
-    BookCategory.SCIENCE_FICTION: [
-        BookRecommendation(
-            id=4, title="The Hitchhiker's Guide to the Galaxy"
-        ),
-        BookRecommendation(id=5, title="Ender's Game"),
-        BookRecommendation(id=6, title="The Dune Chronicles"),
-    ],
-    BookCategory.SELF_HELP: [
-        BookRecommendation(
-            id=7, title="The 7 Habits of Highly Effective People"
-        ),
-        BookRecommendation(
-            id=8, title="How to Win Friends and Influence People"
-        ),
-        BookRecommendation(id=9, title="Man's Search for Meaning"),
-    ],
-}
+class Greeter(hello_pb2_grpc.GreeterServicer):
+    def __init__(self):
+        self.src_dir: str = os.path.dirname(os.path.dirname(__file__))
+        self.data_dir: str = os.path.join(self.src_dir, "Data")
+        self.uploads_dir: str = os.path.join(self.data_dir, "uploads")
+        self.chunk_size: int = 1024
 
-# 將剛剛實作完的service架起來
+    def SayHello(self, request, context):
+        return hello_pb2.StringResponse(message=f'Hello, {request.name}! Your age is {request.age}')
+
+    def UploadFile(self, request_iterator, context):
+        data: bytearray = bytearray()
+        for request in request_iterator:
+            if request.metadata.filename and request.metadata.extension:
+                filepath = get_filepath(
+                    request.metadata.filename, request.metadata.extension)
+                continue
+            data.extend(request.chunk_data)
+        with open(os.path.join(self.uploads_dir, filepath), 'wb') as file:
+            file.write(data)
+        return hello_pb2.StringResponse(message=f'File {filepath} uploaded.')
+
+    def DownloadFile(self, request, context):
+
+        filepath = get_filepath(request.filename, request.extension)
+        if os.path.exists(os.path.join(self.uploads_dir, filepath)):
+            with open(os.path.join(self.uploads_dir, filepath), 'rb') as file:
+                while True:
+                    chunk = file.read(self.chunk_size)
+                    if chunk:
+                        yield hello_pb2.FileResponse(chunk_data=chunk)
+                    else:
+                        return
 
 
 def server(args: list[str]):
@@ -59,12 +125,8 @@ def server(args: list[str]):
     connect_address: str = f"{host}:{port}"
 
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    add_RecommendationsServicer_to_server(
-        RecommendationService(), server
-    )
+    hello_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
     print(f"Server start at {connect_address}")
     server.add_insecure_port(connect_address)
     server.start()
     server.wait_for_termination()
-# if __name__ == "__main__":
-#     serve()  # run gRPC server
